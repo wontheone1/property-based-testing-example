6fa1c2c729a9372abb1415fcdc16a44345fb4645
Won Seob Seo <pghoou@gmail.com>
2019-10-05 09:54:57 +0300

    Introduce goblin, barbarian, attack

--- src/property_based_testing_example/core.clj
+++ src/property_based_testing_example/core.clj
@@ -1,7 +1,24 @@
 (ns property-based-testing-example.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def goblin
+  {:name   "Goblin"
+   :ap     10
+   :dp     1
+   :hp     15
+   :alive? true})
+
+(def barbarian
+  {:name   "Barbarian"
+   :ap     20
+   :dp     0
+   :hp     50
+   :alive? true})
+
+(defn attack [attacker attacked]
+  (let [damage (- (:ap attacker) (:dp attacked))
+        new-hp (- (:hp attacked) damage)
+        new-alive? (pos? new-hp)]
+    (assoc attacked
+      :hp new-hp
+      :alive? new-alive?)))
--- test/property_based_testing_example/core_test.clj
+++ test/property_based_testing_example/core_test.clj
@@ -1,7 +1,20 @@
 (ns property-based-testing-example.core-test
   (:require [clojure.test :refer :all]
-            [property-based-testing-example.core :refer :all]))
+            [property-based-testing-example.core :refer [attack goblin barbarian]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest attack-test
+  (testing "Goblin attacks Barbarian and gives damage"
+    (is (= (attack goblin barbarian)
+           {:name   "Barbarian"
+            :ap     20
+            :dp     0
+            :hp     40
+            :alive? true})))
+
+  (testing "Barbarian attacks Goblin and Goblin dies"
+    (is (= (attack barbarian goblin)
+           {:name   "Goblin"
+            :ap     10
+            :dp     1
+            :hp     -4
+            :alive? false}))))
